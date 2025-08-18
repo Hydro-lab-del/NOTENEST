40506911bb15249780e73dfa55a86a332c39b330
Junaid Ilyas <h6539187@gmail.com>
2025-08-18 08:18:32 +0500

    fix:filename

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(cors({
  }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login_register.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 
